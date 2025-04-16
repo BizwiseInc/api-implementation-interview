44af0abb05586fbeab2b87235543014577db4e8e
Antonio Clarke <antonio@webveloper.com>
2025-04-16 14:08:25 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the Express APddI!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
