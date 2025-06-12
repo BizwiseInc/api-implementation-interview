23ede55aa6b7838b685f5dbec54041d94b13a37a
Antonio Clarke <antonio@webveloper.com>
2025-06-12 16:26:51 -0400

    adds example of user-id

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,16 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Welcome to the Express API!' });
 });
 
+app.post('/', (req: Request, res: Response) => {
+  // Example of how to get the user-id from the headers
+  const userId = req.headers['user-id'];
+
+  res.json({ 
+    userId,
+    status: 'Message received successfully'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 }); 
